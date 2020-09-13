23c44a031d67c10c67b461bd0913355e115beb3f
OlawaleJoseph <adedekojoseph@gmail.com>
2020-09-13 01:18:46 +0100

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="app">
+      <Header />
       <h1>Youtube Clone!!!</h1>
     </div>
   );
